e296f0af191d9d4c7987a39eb84b9550baf12dc8
KrauserHuang <crescentdence@gmail.com>
2023-09-12 09:47:53 +0800

    Initial Commit

---
+++ Tip-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip-Calculator
+//
+//  Created by Tai Chin Huang on 2023/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
