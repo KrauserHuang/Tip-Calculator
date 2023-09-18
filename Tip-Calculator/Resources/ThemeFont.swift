bfe4048b3acf6d9455da7064dda891aacedd6f2e
KrauserHuang <crescentdence@gmail.com>
2023-09-18 09:45:46 +0800

    First commit, create Resources for extension/theme color/theme font

--- Tip-Calculator/Resources/ThemeFont.swift
+++ Tip-Calculator/Resources/ThemeFont.swift
@@ -5,4 +5,20 @@
 //  Created by Tai Chin Huang on 2023/9/12.
 //
 
-import Foundation
+import UIKit
+
+
+struct ThemeFont {
+    // AvenirNext
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demibold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "AvenirNext-DemiBold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
--- Tip-Calculator/View/BillInputView.swift
+++ Tip-Calculator/View/BillInputView.swift
@@ -5,4 +5,29 @@
 //  Created by Tai Chin Huang on 2023/9/12.
 //
 
-import Foundation
+import UIKit
+
+final class BillInputView: UIView {
+    
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
+    
+    init() { // will use autolayout, don't need to care about frames
+        super.init(frame: .zero)
+        setupUI()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Could not create BillInputView")
+    }
+    
+    private func setupUI() {
+        backgroundColor = .red
+    }
+    
+    private func setupConstraint() {
+        
+    }
+}
