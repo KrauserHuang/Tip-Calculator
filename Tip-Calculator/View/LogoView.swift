a0a91857ef092e4ddac6cb43365b82cb5a77102b
KrauserHuang <crescentdence@gmail.com>
2023-09-18 10:36:00 +0800

    create LabelFactory and create LogoView

--- Tip-Calculator/View/LogoView.swift
+++ Tip-Calculator/View/LogoView.swift
@@ -6,8 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
 final class LogoView: UIView {
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView(image: UIImage(named: "icCalculatorBW"))
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    private let topLabel: UILabel = {
+        let label = UILabel()
+        let text = NSMutableAttributedString(
+            string: "Mr TIP",
+            attributes: [.font: ThemeFont.demibold(ofSize: 16)])
+        text.addAttributes([.font: ThemeFont.bold(ofSize: 24)], range: NSMakeRange(3, 3))
+        label.attributedText = text
+        return label
+    }()
+    
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: "Calculator",
+            font: ThemeFont.demibold(ofSize: 20),
+            textAlignment: .left)
+    }()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            topLabel,
+            bottomLabel
+        ])
+        stackView.axis = .vertical
+        stackView.spacing = -4
+        return stackView
+    }()
+    
+    private lazy var hStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            vStackView
+        ])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 8
+        return stackView
+    }()
     
 //    override init(frame: CGRect) {
 //        super.init(frame: frame)
@@ -28,6 +73,12 @@ final class LogoView: UIView {
     }
     
     private func setupConstraint() {
+        hStackView.snp.makeConstraints { make in
+            make.top.bottom.centerX.equalToSuperview()
+        }
         
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(imageView.snp.width)
+        }
     }
 }
