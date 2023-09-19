43c92cc079a20c3aa7a9fd2404798025243d6cad
KrauserHuang <crescentdence@gmail.com>
2023-09-19 14:21:14 +0800

    create BillInputView and addCornerRadius on UIView+ extension

--- Tip-Calculator/View/BillInputView.swift
+++ Tip-Calculator/View/BillInputView.swift
@@ -6,12 +6,54 @@
 //
 
 import UIKit
+import SnapKit
 
 final class BillInputView: UIView {
     
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
+    private let headerView: HeaderView = {
+        return HeaderView()
+    }()
+    
+    private let textFieldContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addCornerRadius(radius: 8.0)
+        return view
+    }()
+    
+    private let currencyDenominationLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: "$",
+            font: ThemeFont.demibold(ofSize: 24))
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return label
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.font = ThemeFont.demibold(ofSize: 28)
+        textField.keyboardType = .decimalPad
+        textField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        textField.tintColor = ThemeColor.text
+        textField.textColor = ThemeColor.text
+        // Add toolbar
+        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: 36))
+        toolBar.barStyle = .default
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(
+            title: "Done",
+            style: .plain,
+            target: self,
+            action: #selector(doneButtonTapped))
+        toolBar.items = [
+            UIBarButtonItem.flexibleSpace(),
+            doneButton
+        ]
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+        return textField
+    }()
     
     init() { // will use autolayout, don't need to care about frames
         super.init(frame: .zero)
@@ -24,10 +66,55 @@ final class BillInputView: UIView {
     }
     
     private func setupUI() {
-        backgroundColor = .red
+        [headerView, textFieldContainerView].forEach { addSubview($0) }
+        textFieldContainerView.addSubview(currencyDenominationLabel)
+        textFieldContainerView.addSubview(textField)
     }
     
     private func setupConstraint() {
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalTo(textFieldContainerView.snp.centerY)
+            make.width.equalTo(68)
+            make.height.equalTo(24)
+            make.trailing.equalTo(textFieldContainerView.snp.leading).offset(-24)
+        }
         
+        textFieldContainerView.snp.makeConstraints { make in
+            make.top.trailing.bottom.equalToSuperview()
+        }
+        
+        currencyDenominationLabel.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(textFieldContainerView.snp.leading).offset(16)
+        }
+        
+        textField.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.equalTo(currencyDenominationLabel.snp.trailing).offset(16)
+            make.trailing.equalTo(textFieldContainerView.snp.trailing).offset(-16)
+        }
+    }
+    
+    @objc
+    private func doneButtonTapped() {
+        textField.endEditing(true)
+    }
+}
+
+class HeaderView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Could not create HeaderView")
+    }
+    
+    private func setupUI() {
+        backgroundColor = .red
     }
 }
