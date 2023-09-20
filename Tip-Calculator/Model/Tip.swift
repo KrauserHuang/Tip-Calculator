4e60b83b09dec097e6b200630cc4f261809b249d
KrauserHuang <crescentdence@gmail.com>
2023-09-20 15:13:13 +0800

    create BillInputView/TipInputView/HeaderView and create Tip Model

---
+++ Tip-Calculator/Model/Tip.swift
@@ -0,0 +1,8 @@
+//
+//  Tip.swift
+//  Tip-Calculator
+//
+//  Created by Tai Chin Huang on 2023/9/20.
+//
+
+import Foundation
---
+++ Tip-Calculator/View/HeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderView.swift
+//  Tip-Calculator
+//
+//  Created by Tai Chin Huang on 2023/9/20.
+//
+
+import Foundation
